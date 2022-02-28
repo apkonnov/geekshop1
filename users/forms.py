c332882c9c9ea9cdb14627b919c923a23a843f7b
apkonnov <a.p.konnov@mail.ru>
2022-02-28 18:03:14 +0400

    lesson4

--- users/forms.py
+++ users/forms.py
@@ -32,3 +32,9 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2')
+
+    def clean_first_name(self):
+        return self.cleaned_data['first_name'].title()
+
+    def clean_last_name(self):
+        return self.cleaned_data['last_name'].title()
